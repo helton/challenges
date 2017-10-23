c6c1111e074d5bcf858e0fd2e872e51fe5a867c4
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2017-10-23 02:16:52 -0200

    Refactoring

---
+++ uri-online-judge/1050 - DDD/cpp/tests.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+source ../../utils/test-utils.sh
+
+testBrasilia() {
+  checkAnswer 61 "Brasilia"
+}
+
+testSalvador() {
+  checkAnswer 71 "Salvador"
+}
+
+testSaoPaulo() {
+  checkAnswer 11 "Sao Paulo"
+}
+
+testRioDeJaneiro() {
+  checkAnswer 21 "Rio de Janeiro"
+}
+
+testJuizDeFora() {
+  checkAnswer 32 "Juiz de Fora"
+}
+
+testCampinas() {
+  checkAnswer 19 "Campinas"
+}
+
+testVitoria() {
+  checkAnswer 27 "Vitoria"
+}
+
+testBeloHorizonte() {
+  checkAnswer 31 "Belo Horizonte"
+}
+
+testDddNaoCadastrado() {
+  checkAnswer 00 "DDD nao cadastrado"
+}
+
+build
+
+printGreen "> Running tests..."
+. ../../utils/shunit2.sh
